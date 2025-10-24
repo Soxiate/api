1567c86acf4996c0b16686b848be65107ee70715
Johannes Tiyasi <johannes.tiyasi@ioco.tech>
2025-10-25 00:31:55 +0200

    feat: add imports for Prisma, Chat, and SocketManager modules in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ChatModule } from './chat/chat.module';
+import { SocketManagerModule } from './socket-manager/socket-manager.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, ChatModule, SocketManagerModule],
   controllers: [AppController],
   providers: [AppService],
 })
